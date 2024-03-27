6a6edfa927f6ebc76a0408e48efc65cbbd2e1fd8
anlyyao <anlyyao@tencent.com>
2024-03-27 20:47:44 +0800

    chore: animation

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Skyline 适配问题记录
 1. border 实现三角形有分界线
 2. 超出省略无效
 3. flex-direction: row-reverse 表现异常
+6. animation 无效
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,7 +1,33 @@
 Component({
   data: {
-    value: [0, 1],
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0,
+    animation: {},
   },
-  methods: {
-  }
-})
+
+  ready() {
+    this.setData({
+      animation: wx
+        .createAnimation({ duration: 3000, timingFunction: 'linear', delay: 500 })
+        .rotate(150)
+        .scale(3)
+        .step({ duration: 8000 })
+        .export(),
+    });
+    // this.setData({
+    //   animation: wx
+    //     .createAnimation({ duration: 3000, timingFunction: 'linear', delay: 500 })
+    //     .translateX(-100)
+    //     .step()
+    //     .export(),
+    // });
+  },
+  methods: {},
+});
